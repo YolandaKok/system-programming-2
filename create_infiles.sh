1395d110ca7018b912a62fdb7f39c1875c33d66c
YolandaKok <yolanda.kokkinou@gmail.com>
2019-03-24 23:13:35 +0200

    Round Robin for files

--- create_infiles.sh
+++ create_infiles.sh
@@ -41,7 +41,10 @@ fi
 declare -a Directory_Names
 
 
-Directory_Names=($(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3 | head -n $3))
+for((i=0;i<num_of_dirs;i++)); do
+    num_of_characters=$(( ( RANDOM % 8 )  + 1 ))
+    Directory_Names[i]=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${num_of_characters} | head -n 1)
+done
 
 # Loop the directory names
 for((i=0;i<$num_of_dirs;i++)); do
@@ -50,12 +53,17 @@ done
 
 current_level=0
 current_directory="$dir_name"
+
+declare -a Directory_Names_New
+
+Directory_Names_New[0]=${current_directory}
+
 # For every directory
 for((i=0;i<$num_of_dirs;i++)); do
-    #current_directory="$dir_name/${Directory_Names[i]}"
     if [[ "$current_level" -lt "$levels" ]]; then
         current_directory="$current_directory/${Directory_Names[i]}"
-        echo $current_directory
+        echo ${current_directory}
+        Directory_Names_New[i+1]=${current_directory}
         mkdir -p ${current_directory};
         current_level=$((current_level+1))
     fi
@@ -63,5 +71,27 @@ for((i=0;i<$num_of_dirs;i++)); do
         current_directory="$dir_name"
         current_level=0
     fi
-    #directory="$dir_name/${Directory_Names[i]}"
 done
+
+declare -a File_Names
+
+# Create the random file names
+for((i=0;i<num_of_files;i++)); do
+    num_of_characters=$(( ( RANDOM % 8 )  + 1 ))
+    File_Names[i]=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${num_of_characters} | head -n 1)
+    echo ${File_Names[i]}
+done
+
+j=0
+current_directory="$dir_name"
+# Round Robin to put the files
+for((i=0;i<$num_of_files;i++)); do
+    path_to_file="${Directory_Names_New[j]}/${File_Names[i]}"
+    touch ${path_to_file}
+    echo ${path_to_file}
+    j=$((j+1))
+    if [[ "$j" -eq "$num_of_dirs+1" ]]; then
+        j=0
+    fi
+done
+
