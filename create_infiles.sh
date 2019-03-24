4f86d57ddd7015ba07582c6e4a5b6eb527aadc28
YolandaKok <yolanda.kokkinou@gmail.com>
2019-03-24 21:57:07 +0200

    created the hierarchy for the directories

---
+++ create_infiles.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Check input parameters
+
+# Directory Name
+dir_name=$1
+# Number of files that we want to create
+num_of_files=$2
+# Num of Directories
+num_of_dirs=$3
+# Levels of the directories
+levels=$4
+
+# Check if num_of_files and num_of_dirs are greater than zero
+if [[ "$2" -lt "0" ]] || [[ "$2" -eq "0" ]]; then
+    echo Error with number of files
+    exit
+fi
+
+if [[ "$3" -lt "0" ]] || [[ "$3" -eq "0" ]]; then
+    echo Error with number of directories
+    exit
+fi
+
+# Check the number of levels
+if [[ "$4" -lt "0" ]] || [[ "$4" -eq "0" ]]; then
+    echo Levels have to be greater than 1
+    exit
+fi
+
+# Check if directory exists
+
+if [[ -d ${dir_name} ]]; then
+    echo Directory already exists !
+    exit
+else
+    # If does not exist, create the directory
+    mkdir -p ${dir_name};
+fi
+
+declare -a Directory_Names
+
+
+Directory_Names=($(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3 | head -n $3))
+
+# Loop the directory names
+for((i=0;i<$num_of_dirs;i++)); do
+    echo ${Directory_Names[i]}
+done
+
+current_level=0
+current_directory="$dir_name"
+# For every directory
+for((i=0;i<$num_of_dirs;i++)); do
+    #current_directory="$dir_name/${Directory_Names[i]}"
+    if [[ "$current_level" -lt "$levels" ]]; then
+        current_directory="$current_directory/${Directory_Names[i]}"
+        echo $current_directory
+        mkdir -p ${current_directory};
+        current_level=$((current_level+1))
+    fi
+    if [[ "$current_level" -eq "$levels" ]]; then
+        current_directory="$dir_name"
+        current_level=0
+    fi
+    #directory="$dir_name/${Directory_Names[i]}"
+done
