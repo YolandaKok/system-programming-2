37f387c4fe3a31bdca2a5c9e63e8ab6160b51eb4
YolandaKok <yolanda.kokkinou@gmail.com>
2019-03-31 14:34:36 +0300

    Total num of files sent, received

--- get_stats.sh
+++ get_stats.sh
@@ -1,26 +1,60 @@
 #!/bin/bash
 
 count=0
-bytes_send=0
+
+bytes_sent=0
+flag1=0
+
 bytes_received=0
 flag=0
-two=2
+
+flag2=0
+current_id=-1
+
+num_of_sent=0
+num_of_received=0
+
 while read line; do
     # if it is the first line
     for word in ${line}; do
-        echo "word = '$word'"
+        #echo "word = '$word'"
+        if [[ ${word} == "Client" ]]; then
+            flag2=1
+        fi
+
+        if [[ ${flag2} -eq "1" ]] && [[ ${count} -eq "1" ]]; then
+            flag2=0
+            if [[ ${word} -gt ${current_id} ]]; then
+                current_id=${word}
+            fi
+        fi
+
         if [[ ${word} == "Received" ]]; then
             flag=1
         fi
+
         if [[ ${flag} -eq "1" ]] && [[ ${count} -eq "2" ]]; then
             flag=0
             bytes_received=$((bytes_received+word))
-            echo ${word}
+            num_of_received=$((num_of_received+1))
+        fi
+
+        if [[ ${word} == "Sent" ]]; then
+            flag1=1
+        fi
+
+        if [[ ${flag1} -eq "1" ]] && [[ ${count} -eq "2" ]]; then
+            flag1=0
+            bytes_sent=$((bytes_sent+word))
+            num_of_sent=$((num_of_sent+1))
         fi
         count=$((count+1))
     done
     count=0
 done
 
-echo ${bytes_received}
-
+echo "Greatest id: ${current_id}"
+echo "Total bytes sent: ${bytes_sent}"
+echo "Total bytes received: ${bytes_received}"
+echo "Number of files sent: ${num_of_sent}"
+echo "Number of files received: ${num_of_received}"
