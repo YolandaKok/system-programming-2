510d4800c2ac28f3981b9ad479174cc87ca93d18
YolandaKok <yolanda.kokkinou@gmail.com>
2019-04-07 16:13:14 +0300

    Completed get stats script

--- get_stats.sh
+++ get_stats.sh
@@ -16,18 +16,33 @@ num_of_received=0
 
 count_exit=0
 
+num_of_clients=0
+
+declare -a clients_list
+
+
 while read line; do
     # if it is the first line
     for word in ${line}; do
-        #echo "word = '$word'"
         if [[ ${word} == "Client" ]]; then
             flag2=1
+            num_of_clients=$((num_of_clients+1))
         fi
 
         if [[ ${flag2} -eq "1" ]] && [[ ${count} -eq "1" ]]; then
             flag2=0
             if [[ ${word} -gt ${current_id} ]]; then
                 current_id=${word}
+            fi
+            if [[ ${num_of_clients} -eq "1" ]]; then
+                clients_list[${num_of_clients}-1]=${word}
+                minimum_client=${word}
+            fi
+            if [[ ${num_of_clients} -gt "1" ]]; then
+                clients_list[${num_of_clients}-1]=${word}
+                if [[ ${minimum_client} -gt ${word} ]]; then
+                    minimum_client=${word}
+                fi
             fi
         fi
 
@@ -60,7 +75,10 @@ while read line; do
     count=0
 done
 
-echo "Greatest id: ${current_id}"
+echo "Client Ids: ${clients_list[@]}"
+echo "Number of clients which connected to the network is: ${num_of_clients}"
+echo "Minimum client id: ${minimum_client}"
+echo "Greatest client id: ${current_id}"
 echo "Total bytes sent: ${bytes_sent}"
 echo "Total bytes received: ${bytes_received}"
 echo "Number of files sent: ${num_of_sent}"
